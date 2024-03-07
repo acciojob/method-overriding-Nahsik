95bccb6ba01470f400fd34fffe4280fb82b5c983
kishan <kishan.dubariya@phenompeople.com>
2024-03-07 23:13:26 +0530

    first commnit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static class A {
+
+        public static String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        public static String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
 }
